f6a14df902038ab1bcee9b4008d511aa21de83e5
Richard Aber <richaber@gmail.com>
2018-04-17 16:52:00 -0500

    Add some useful FILE, DIR, and URL constants to plugin bootstrap

--- index.php
+++ index.php
@@ -15,6 +15,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * The full path and filename of this bootstrap file with symlinks resolved.
+ *
+ * @var string GUTYBLOCKS_BOOTSTRAP_FILE
+ */
+define( 'GUTYBLOCKS_BOOTSTRAP_FILE', __FILE__ );
+
+/**
+ * The full path to the parent directory of this bootstrap file with symlinks resolved, with trailing slash.
+ *
+ * @var string GUTYBLOCKS_DIR
+ */
+define( 'GUTYBLOCKS_DIR', dirname( GUTYBLOCKS_BOOTSTRAP_FILE ) . '/' );
+
+/**
+ * The relative path to this plugin directory, from WP_PLUGIN_DIR, with trailing slash.
+ *
+ * @var string GUTYBLOCKS_REL_DIR
+ */
+define( 'GUTYBLOCKS_REL_DIR', basename( GUTYBLOCKS_DIR ) . '/' );
+
+/**
+ * The URL of the plugin directory, with trailing slash.
+ *
+ * Example: https://example.local/wp-content/plugins/hcmc-custom-objects/
+ *
+ * @const string GUTYBLOCKS_URL
+ */
+define( 'GUTYBLOCKS_URL', plugins_url( '/', GUTYBLOCKS_BOOTSTRAP_FILE ) );
+
 // Automatically load all "blocks" -- New blocks are required to have an index.php file in order to be loaded.
 foreach ( glob( dirname( __FILE__ ) . '/blocks/*/index.php' ) as $block_logic ) {
     require $block_logic;
