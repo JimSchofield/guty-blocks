59a55b5353036859246b8895fa39b866201fefb6
Richard Aber <richaber@gmail.com>
2018-04-18 15:47:14 -0500

    Adding example POT, PO, and MO files from i18n work

---
+++ languages/guty-blocks-js-translations.php
@@ -0,0 +1,7 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$generated_i18n_strings = array(
+	// Reference: src/hello-world/hello-world.src.js:35
+	__( 'Hello World!', 'guty-blocks' )
+);
+/* THIS IS THE END OF THE GENERATED FILE */
